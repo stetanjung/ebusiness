cb538ad5e080de695df0a857e236d4cbdc7effbf
stephentanjung <stephentanjung@ruangguru.com>
2018-11-08 04:09:45 +0800

    add-templates-folder

--- index/views.py
+++ index/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def indexView(request):
+    #return HttpResponse('hello this is a test')
+    return render(request, 'index.html')
