7ee00211fdb555fe105ebf185f0e57b5e1ffdb00
stephentanjung <stephentanjung@ruangguru.com>
2018-11-13 19:55:39 +0800

    initial commit

--- index/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def indexView(request):
-    #return HttpResponse('hello this is a test')
-    return render(request, 'index.html')
