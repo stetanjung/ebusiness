5cb7ee0f34954c88c9ee2d07224f483b058060cf
stephentanjung <stephentanjung@ruangguru.com>
2018-11-15 00:07:31 +0800

    add backend (connection and tableCreation)

---
+++ homehelper/backend/connection.php
@@ -0,0 +1,13 @@
+<?php
+    $SERVERNAME = "mysql.comp.polyu.edu.hk";
+    $USERNAME = /** student id */"";
+    $PASSWORD = /** password */"";
+    $SCHEMA = /** default schema (student id) */"";
+ 
+    $con = new mysqli($SERVERNAME, $USERNAME, $PASSWORD, $SCHEMA);
+
+    if(!$con){
+        die("Connection Failed: ".mysqli_connect_error());
+        echo("Please contact the admin");
+    }
+?>
