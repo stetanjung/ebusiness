b7eeea109e41d0385b973a072db6ed96555d9148
stephentanjung <stephentanjung@ruangguru.com>
2018-11-16 00:35:47 +0800

    finish the table creation by martin

--- homehelper/backend/tableCreation.php
+++ homehelper/backend/tableCreation.php
@@ -18,23 +18,44 @@
     )";
 
     $comment = "CREATE TABLE comment (
-
+        commentID int unsigned auto_increment primary key,
+        paymentID int unsigned not null,
+        userID varchar(30) not null,
+        content varchar(200) not null,
     )";
 
     $employee = "CREATE TABLE empolyee (
-
+        eployeeID int unsigned auto_increment primary key,
+        employeeName varchar(30) not null,
+        contactNumber varchar(20) not null,
+        age int unsigned not null,
+        gender varchar(1) not null,
     )";
 
     $serviceType = "CREATE TABLE service_type (
-
+        serviceID int unsigned auto_increment primary key,
+        employeeID varchar(30) not null,
+        typeName varchar(30) not null,
+        price int unsigned not null,
+        availableTime int unsigned not null,
+        description int unsigned not null,
     )";
 
     $shoppingCart = "CREATE TABLE shopping_cart (
-
+        cartID int usigned auto_increment primary key,
+        userID int unsigned not null,
+        serviceID int unsigned not null,
+        paymentID int unsigned not null,
     )";
 
     $payment = "CREATE TABLE payment (
-
+        paymentID int unsigned auto_increment primary key,
+        cartID int unsigned not null,
+        userID int unsigned not null,
+        creditCard int varchar(16) not null,
+        address int varchar(200) not null,
+        time int varchar(20) not null,
+        totalPrice unsigned not null,
     )";
 
     $createTable = array();
