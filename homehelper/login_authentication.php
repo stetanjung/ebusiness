90e0412cb764ea130e4bcac21f982678bb5bcc28
stephentanjung <stephentanjung@ruangguru.com>
2018-11-25 15:51:34 +0800

    login and signup process

--- homehelper/login_authentication.php
+++ homehelper/login_authentication.php
@@ -13,9 +13,19 @@
     if($error){
         echo("<script>alert('All fields are required.');window.location.href='/ebusiness/homehelper/login.php';</script>");
     }
-
-    $_SESSION['usersID'] = '';
-
+    else{
+        $username = $_POST['username'];
+        $password = md5($_POST['password']);
+        $result=mysqli_query($con,"SELECT users_id FROM users WHERE username='$username' AND password='$password'");
+        if(mysqli_num_rows($result)==0) {
+            echo("<script>alert('User is not exist.');window.location.href='/ebusiness/homehelper/login.php';</script>");
+        }
+        else{
+            $array=mysqli_fetch_array($result,MYSQLI_ASSOC);
+            $_SESSION['users_id']=$array['users_id'];
+            
+        }
+    }
     echo("<script>window.location.href=' /comp2121/assignment2/phoneFood'</script>");
     include('connectionclose.php');
 ?>
