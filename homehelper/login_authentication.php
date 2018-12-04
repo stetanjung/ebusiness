6047b33fddcf9db635ac36af061c1e023591150b
stephentanjung <stephentanjung@ruangguru.com>
2018-12-04 19:57:13 +0800

    update

--- homehelper/login_authentication.php
+++ homehelper/login_authentication.php
@@ -28,24 +28,24 @@
         }
     }
     if($error && $resp == true){
-        echo("<script>alert('All fields are required.');window.location.href='/~16094653d/ebusiness/homehelper/login.php';</script>");
+        echo("<script>alert('All fields are required.');window.location.href='login.php';</script>");
     }
     else{
         $username = $_POST['username'];
         $password = md5($_POST['password']);
         $result=mysqli_query($con,"SELECT userID FROM user WHERE username='$username' AND pass='$password'");
         if(mysqli_num_rows($result) == 0) {
-            echo("<script>alert('User is not exist. $password');window.location.href='/~16094653d/ebusiness/homehelper/login.php';</script>");
+            echo("<script>alert('User is not exist. $password');window.location.href='login.php';</script>");
         }
         else{
             $array=mysqli_fetch_array($result,MYSQLI_ASSOC);
             if($array['userID'] == '1'){
                 $_SESSION['users_id']=$array['userID'];
-                echo("<script>window.location.href=' /~16094653d/ebusiness/homehelper/admin.html'</script>");            
+                echo("<script>window.location.href='admin.html'</script>");            
             }
             $_SESSION['users_id']=$array['userID'];
         }
     }
     include('connectionclose.php');
-    echo("<script>window.location.href=' /~16094653d/ebusiness/homehelper/'</script>");
+    echo("<script>window.location.href=' index.php'</script>");
 ?>
