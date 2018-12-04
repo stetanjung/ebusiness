fe38643c1788718b51ff0369fbd7ee9623cb84fe
YU Jing <yuki5834444@gmail.com>
2018-12-04 16:45:14 +0800

    add order history frontend

--- homehelper/footer.php
+++ homehelper/footer.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Footer</title>
-</head>
-<body>
-    
-</body>
-</html>
+<footer>
+	<div class="footer">
+		<div id="copyright">
+			<p>Copyright © 2018 HomeHelper. All Rights Reserved.</p>
+		</div>
+	</div>
+</footer>
