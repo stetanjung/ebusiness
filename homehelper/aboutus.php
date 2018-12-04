481539cb7e172e7b30500792d2b623d3b0f27cca
Martin TANG <44494311+fishbelly8925@users.noreply.github.com>
2018-12-04 21:22:05 +0800

    Update aboutus.php

---
+++ homehelper/aboutus.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+    <head>
+    <meta charset="UTF-8">
+    <title>HomeHelper</title>
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="normalize.css">
+    </head>
+    <body>
+        <?php
+		require('nav.php');
+        ?>
+        <blockquote class="text">
+            <p>
+            Home-helper is a profitable service-oriented B2C company,
+            which focus on providing<br>hourly domestic services,
+            such as caretaking, houseworking, and venue-cleaning.<br>
+            We uphold a value,“Your wish is our command!” In order to
+            live up to the standard,<br>after being hired, our employees
+            would receive futher train so that they could acquire<br> not merely
+            basic requirement but advanced skills.
+            <p>
+            After further-training, our domestic workers are equipped with
+            high-level knowledge<br>which enable them to utilize their adept
+            technique to fulfill what you think about your<br>home enviroment.
+            That is, customers could make special request and additional orders<br>
+            to the workers; then, our company would provide customized service
+            through<br>the assistance of our workers who have relative skills. On
+            top of that, our staffs has<br>been trained to some extent to take care
+            of everything and hence with no doubt, our<br>customers could rely their
+            on the hands of the domestic workers and feel at ease with<br> our service.
+
+            <p>
+            It is quite convenient for customers to access domestic services from
+            our company<br>–– All they need to do is login to our user-friendly website
+            and choose which services<br>they feel interested in. After doing so, customers
+            could proceed on choosing the helper<br>that they want and complete the payment.
+            Then, customers could just sit back and relax<br>themselves until the helper arrives
+            in the door step after a couple of hours and enjoy the<br>services provision.
+        </blockquote>
+    </body>
+
+</html>
